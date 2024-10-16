161e075964de468adec74416f425c14c7393ec4d
PoyarkovA <mr.sanya.poyarkov@gmail.com>
2024-10-16 20:07:43 +0500

    Add files via upload

---
+++ examples/example1.py
@@ -0,0 +1,13 @@
+print("Определение и вызов функции")
+print("Пример простейшей функции без параметров: ")
+def example():
+    print("Добрый вечер!")
+example()
+
+print("Использование ключевого слова return: ")
+def example():
+    return True
+example = example()
+print(type(example))
+print(example)
+    
